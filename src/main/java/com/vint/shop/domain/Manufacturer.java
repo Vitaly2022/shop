583c576b397cc5587975703a6de23799175c9f82
VITALY <9004673@mail.ru>
2023-02-04 19:00:43 +0300

    First bug fixed

--- src/main/java/com/vint/shop/domain/Manufacturer.java
+++ src/main/java/com/vint/shop/domain/Manufacturer.java
@@ -1,10 +1,9 @@
 package com.vint.shop.domain;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
-public class Supplier { // toString
+public class Manufacturer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -14,12 +13,9 @@ public class Supplier { // toString
     @Column(name = "name")
     private String name;
 
-
-    @Column(name = "email")
-    private String email;
-
     @ManyToOne (fetch = FetchType.LAZY)
     @JoinColumn (name = "product_id")
     private Product product;
+
 
 }
---
+++ src/main/java/com/vint/shop/repository/Supplier.java
@@ -0,0 +1,6 @@
+package com.vint.shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Supplier extends JpaRepository <Supplier, Integer> {
+}
